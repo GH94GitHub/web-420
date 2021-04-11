d5411b67015da58051e7a52ae27b96a1cd920227
Tony Henderson <th_rams94@hotmail.com>
2021-04-11 12:58:33 -0500

    code cleanup

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -7,16 +7,18 @@
 
 var createError = require('http-errors');
 var express = require('express');
-var mongoose = require('mongoose');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 
+/* Routes */
 var apiCatalog = require('./routes/api-catalog');
 var indexRouter = require('./routes/index');
 
 mongoose.Promise = require('bluebird');
-mongoose.connect('mongodb+srv://admin:admin@buwebdev-cluster-1.qqxjy.mongodb.net/api-gateway?authSource=admin&replicaSet=atlas-njj3c5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', { //!fix string
+mongoose.connect('mongodb+srv://admin:admin@buwebdev-cluster-1.qqxjy.mongodb.net/api-gateway?authSource=admin&replicaSet=atlas-njj3c5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true', {
   promiseLibrary: require('bluebird')
 }).then(() => console.log('connection successful')).catch((err) => console.error(err));
 
@@ -27,10 +29,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cookieParser());
 
 
 app.use('/', indexRouter);
