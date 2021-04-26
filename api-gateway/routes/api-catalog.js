ee993e79c308716aeb1b1bfb6793f7051efbd8bd
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-04-25 21:11:19 -0500

    Added login & logout

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -9,10 +9,17 @@ var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controller/authController');
 
+// POST request for logging in a user
+router.post('/auth/login', auth_controller.user_login);
+
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register)
 
+
 // GET request for verifying user tokens
 router.get('/auth/token', auth_controller.user_token);
+
+// GET request for logging out a user
+router.get('/auth/logout', auth_controller.user_logout);
 
 module.exports = router;
