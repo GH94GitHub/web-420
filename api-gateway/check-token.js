94c9e8a113ae1794b3264c1f49167ff90c473b39
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2021-05-09 09:27:32 -0500

    Initial commit

---
+++ api-gateway/check-token.js
@@ -0,0 +1,25 @@
+/*===========================
+; Title: Check Token
+; Author: George Henderson
+; Date: 9 May 2021
+; Description: Checks user token
+;===========================*/
+
+var jwt = require('jsonwebtoken');
+var config = require('./config');
+
+function checkToken(req, res, next) {
+    var token = req.headers['x-access-token'];
+
+    if (!token)
+        return res.status(403).send({ auth: false, message: 'No token provided.'})
+
+    jwt.verify(token, config.web.secret, function(err, decoded) {
+        if (err) return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+module.exports = checkToken
